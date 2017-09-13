482a53d5ca9cee841b91e233d8251c5f15cea7b5
Ps-Pencil <pan.song@dhs.sg>
2017-09-13 15:10:42 +0200

    update

--- deploy.go
+++ deploy.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -24,65 +25,34 @@ var (
 // CopyFile copies a file from src to dst. If src and dst files exist, and are
 // the same, then return success. Otherise, attempt to create a hard link
 // between the two files. If that fail, copy the file contents from src to dst.
-func CopyFile(src, dst string) (err error) {
+func cp(src, dst string) (err error) {
 	sfi, err := os.Stat(src)
 	if err != nil {
 		return
 	}
-	if !sfi.Mode().IsRegular() {
-		// cannot copy non-regular files (e.g., directories,
-		// symlinks, devices, etc.)
-		return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
-	}
-	dfi, err := os.Stat(dst)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			return
-		}
-	} else {
-		if !(dfi.Mode().IsRegular()) {
-			return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dfi.Name(), dfi.Mode().String())
-		}
-		if os.SameFile(sfi, dfi) {
-			return
-		}
-	}
-	if err = os.Link(src, dst); err == nil {
-		return
-	}
-	err = copyFileContents(src, dst)
-	return
-}
 
-// copyFileContents copies the contents of the file named src to the file named
-// by dst. The file will be created if it does not already exist. If the
-// destination file exists, all it's contents will be replaced by the contents
-// of the source file.
-func copyFileContents(src, dst string) (err error) {
-	in, err := os.Open(src)
+	diffcmd := exec.Command("diff", src, dst)
+	cmd := exec.Command("cp", src, dst)
+	if sfi.IsDir() {
+		cmd = exec.Command("cp", "-r", src, dst)
+		diffcmd = exec.Command("diff", "-r", src, dst)
+	}
+
+	_, diff := diffcmd.CombinedOutput()
+	if diff == nil {
+		return nil
+	}
+
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return
+		return fmt.Errorf("Error copying file: %q %s", output, err)
 	}
-	defer in.Close()
-	out, err := os.Create(dst)
-	if err != nil {
-		return
-	}
-	defer func() {
-		cerr := out.Close()
-		if err == nil {
-			err = cerr
-		}
-	}()
-	if _, err = io.Copy(out, in); err != nil {
-		return
-	}
-	err = out.Sync()
-	return
+	return nil
 }
 
 func main() {
 	flag.Parse()
+
 	home := os.Getenv("HOME")
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
@@ -120,7 +90,7 @@ func main() {
 		}
 		fmt.Println(from, to)
 
-		if err = CopyFile(from, to); err != nil {
+		if err = cp(from, to); err != nil {
 			log.Fatalf("Error copying file from %q to %q: %s", from, to, err)
 		}
 	}
